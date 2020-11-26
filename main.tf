a1ce2ed58e889ba0aa37934b5ec24a502b89156c
oguz <oguzzarci@hotmail.com>
2020-11-26 20:33:24 +0300

    minor fix

--- main.tf
+++ main.tf
@@ -62,13 +62,13 @@ resource "aws_instance" "jenkins" {
         "sudo systemctl start jenkins",
         "sudo systemctl enable jenkins",
         "sudo systemctl status jenkins",
-        "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
-        "echo Install EKSCTL"
-        "curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp",
-        "sudo mv /tmp/eksctl /usr/local/bin"
+        "sudo cat /var/lib/jenkins/secrets/initialAdminPassword",
+        "echo Install EKSCTL",
+        "curl --silent --location 'https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz' | tar xz -C /tmp",
+        "sudo mv /tmp/eksctl /usr/local/bin",
         "echo Install KUBECTL",
         "curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl",
-        "chmod +x ./kubectl"
+        "chmod +x ./kubectl",
         "mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin",
         "kubectl version --short --client",
         "Install awsIamAuthenticator",
