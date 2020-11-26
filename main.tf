c2a52aa896b6500826ddb2b48f215052504033b1
oguz <oguzzarci@hotmail.com>
2020-11-26 20:22:36 +0300

    install aws-iam-authenticator

--- main.tf
+++ main.tf
@@ -70,7 +70,13 @@ resource "aws_instance" "jenkins" {
         "curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl",
         "chmod +x ./kubectl"
         "mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin",
-        "kubectl version --short --client"
+        "kubectl version --short --client",
+        "Install awsIamAuthenticator",
+        "curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/aws-iam-authenticator",
+        "chmod +x ./aws-iam-authenticator",
+        "mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin",
+        "echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc"
+
     ]
   }
 
