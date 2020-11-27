b5122d50d82ab8267af15e0e779b4cbe13ed1f5d
oguz <oguzzarci@hotmail.com>
2020-11-27 22:51:00 +0300

    eksctl install fix

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_instance" "jenkins" {
         "sudo systemctl status jenkins",
         "sudo cat /var/lib/jenkins/secrets/initialAdminPassword",
         "echo Install EKSCTL",
-        "curl --silent --location \"https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz\" | tar xz -C /tmp",
+        "curl --silent --location https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz | tar xz -C /tmp",
         "sudo mv /tmp/eksctl /usr/local/bin",
         "echo Install KUBECTL",
         "curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl",
